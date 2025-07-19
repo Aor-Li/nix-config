906c8fc94b37dc08934bc9005b637e81c0f95e0b
aor <liyifeng0039@gmail.com>
2025-07-19 20:54:19 +0800

    .

---
+++ flakes/develope/cpp.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic cpp develope env flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            libgcc
+            make
+            cmake
+          ];
+
+          shellHook = ''
+            echo "cpp开发环境就绪!"
+          '';
+        };
+      }
+    );
+}
--- host/base/nix/default.nix
+++ host/base/nix/default.nix
@@ -1,12 +1,16 @@
 { pkgs, inputs, ... }:
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   system.stateVersion = "25.11";
 
   # config substituters
   nix.settings.substituters = [
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
+    "https://mirrors.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
   ];
 
   # config nix-ld

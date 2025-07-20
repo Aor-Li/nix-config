b24ef1fddaeab3961d7c17c05cac327250d27778
aor <liyifeng0039@gmail.com>
2025-07-21 02:09:45 +0800

    add sops-nix for sercret manage

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
 
+    # sops-nix
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     # # hyperland
     # hyprland.url = "github:hyprwm/Hyprland";
     # hyprland.inputs.nixpkgs.follows = "nixpkgs";
